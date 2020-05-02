47ddfa1ffca804af4f7b9eb028ef9fe1e848a79c
Gabriel Lucas <dreadpool38@gmail.com>
2020-05-02 17:39:20 -0300

    Adicionando css básico e o  campo para digitar o user

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import "./TextArea.css";
 
 export default function TextArea(props) {
   return (
     <div>
+      <TextField
+        label="User"
+        style={{ margin: 8 }}
+        placeholder="What is your name?"
+        margin="normal"
+      />
       <div className="message-area"></div>
       <TextField
         id="standard-full-width"
